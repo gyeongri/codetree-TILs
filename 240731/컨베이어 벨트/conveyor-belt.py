57cf9bd54743ceea4cb5e29e5b7b0e966d66b7e6
gyeongri <50294908+gyeongri@users.noreply.github.com>
2024-07-31 17:43:38 +0900

    Title: 컨베이어 벨트, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240731/컨베이어 벨트/conveyor-belt.py
+++ 240731/컨베이어 벨트/conveyor-belt.py
@@ -1,4 +1,4 @@
-from sys import stdin
+from sys import stdin, stdout
 n,t = map(int, stdin.readline().split())
 
 arr = list(map(int, stdin.readline().split()))
@@ -6,13 +6,17 @@ arr2 = list((map(int, stdin.readline().split())))
 arr.extend(arr2)
 
 t2 = t%(2*n)
-
 new_arr = arr[-t2:]+arr[:n*2-t2]
 
-for i in range(n):
-    print(new_arr[i], end= " ")
+tmp1 = ' '.join(map(str, new_arr[:n]))
+tmp2 = ' '.join(map(str, new_arr[n:2 * n]))
+answer = tmp1 + "\n" + tmp2
+# for i in range(n):
+#     print(new_arr[i], end= " ")
 
-print()
+# print()
 
-for i in range(n, 2*n):
-    print(new_arr[i], end = " ")
+# for i in range(n, 2*n):
+#     print(new_arr[i], end = " ")
+
+stdout.write(answer)
