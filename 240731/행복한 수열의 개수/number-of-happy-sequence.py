2e8306c45577ec0a8f0c1c14e351ef6398539bf6
gyeongri <50294908+gyeongri@users.noreply.github.com>
2024-07-31 11:16:43 +0900

    Title: 행복한 수열의 개수, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240731/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240731/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,29 +1,34 @@
 n, m = map(int,(input().split()))
 map = [list(map(int, input().split())) for _ in range(n)]
 sum = 0
-for i in range(n):
-    cnt = 0
-    for j in range(1,n):
-        
-        if map[i][j]==map[i][j-1]:
-            cnt+=1
-            if cnt == m-1:
-                sum+=1
-                break
-        else:
-            cnt=0
+if m==1:
+    sum=2*n
 
-for j in range(n):
-    cnt = 0
-    #flag = False
-    for i in range(1,n):
-        
-        if map[i][j]==map[i-1][j]:
-            cnt+=1
-            if cnt == m-1:
-                sum+=1
-                break
-        else:
-            cnt=0
+else:
+
+    for i in range(n):
+        cnt = 0
+        for j in range(1,n):
+            
+            if map[i][j]==map[i][j-1]:
+                cnt+=1
+                if cnt == m-1:
+                    sum+=1
+                    break
+            else:
+                cnt=0
+
+    for j in range(n):
+        cnt = 0
+        #flag = False
+        for i in range(1,n):
+            
+            if map[i][j]==map[i-1][j]:
+                cnt+=1
+                if cnt == m-1:
+                    sum+=1
+                    break
+            else:
+                cnt=0
 
 print(sum)
