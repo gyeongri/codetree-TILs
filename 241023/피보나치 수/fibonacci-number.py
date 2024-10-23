dc4242bfcb595702f668904d13808845cfad19ad
gyeongri <50294908+gyeongri@users.noreply.github.com>
2024-10-23 22:40:52 +0900

    Title: 피보나치 수, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 241023/피보나치 수/fibonacci-number.py
+++ 241023/피보나치 수/fibonacci-number.py
@@ -1,16 +1,8 @@
 from sys import stdin, stdout
 N = int(stdin.readline())
-dp = [0 for _ in range(N)]
-dp[0] = 1
-dp[1] = 1
 
+arr = [1 for _ in range(N)]
 for i in range(2,N):
-    dp[i] = dp[i-1]+dp[i-2]
+    arr[i] = arr[i-1]+arr[i-2]
 
-# def fibo(n):
-#     if n == 1:
-#         return 1
-#     else:
-#         return fibo(n-1)
-
-stdout.write(str(dp[N-1]))
+stdout.write(str(arr[N-1]))
