632da1de5aea96076cbff9e165b4e43691263067
gyeongri <50294908+gyeongri@users.noreply.github.com>
2024-11-14 22:42:43 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241114/Date to Date/date-to-date.py
+++ 241114/Date to Date/date-to-date.py
@@ -3,8 +3,11 @@ months = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 
 m1, d1, m2, d2 = map(int, stdin.readline().split())
 
-ans = months[m1] - d1 + 1 + d2
-for i in range(m1+1, m2):
-    ans+=months[i]
+if m1 == m2:
+    ans = d2 - d1 + 1
+else:
+    ans = months[m1] - d1 + 1 + d2
+    for i in range(m1+1, m2):
+        ans+=months[i]
 
 stdout.write(str(ans))
